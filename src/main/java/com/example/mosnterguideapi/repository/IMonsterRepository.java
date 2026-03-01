94f5e68b0d3e0cdea3158affe8eb61f29bcff901
Leticia-SS <leticia.saraiva013@gmail.com>
2026-03-01 20:11:09 -0300

    creating servicee class

--- src/main/java/com/example/mosnterguideapi/repository/IMonsterRepository.java
+++ src/main/java/com/example/mosnterguideapi/repository/IMonsterRepository.java
@@ -3,5 +3,5 @@ package com.example.mosnterguideapi.repository;
 import com.example.mosnterguideapi.model.Monster;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MonsterRepository extends JpaRepository<Monster, Long> {
+public interface IMonsterRepository extends JpaRepository<Monster, Long> {
 }
---
+++ src/main/java/com/example/mosnterguideapi/service/MonsterService.java
@@ -0,0 +1,41 @@
+package com.example.mosnterguideapi.service;
+
+import com.example.mosnterguideapi.model.Monster;
+import com.example.mosnterguideapi.repository.IMonsterRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MonsterService {
+    private final IMonsterRepository monsterRepository;
+
+    public MonsterService(IMonsterRepository monsterRepository) {
+        this.monsterRepository = monsterRepository;
+    }
+
+    public List<Monster> getAllMonsters() {
+        return monsterRepository.findAll();
+    }
+
+    public Optional<Monster> getMonsterById(Long id) {
+        return Optional.ofNullable(monsterRepository.findById(id).orElse(null));
+    }
+
+    public void addMonster(Monster monster){
+        monster.setLevel(1);
+        monsterRepository.save(monster);
+    }
+
+    public void deleteMonster(Long id) {
+        monsterRepository.deleteById(id);
+    }
+
+    public Monster levelUp(Long id){
+        Monster monster = monsterRepository.findById(id).orElseThrow(()->new RuntimeException("Monstro não encontrado"));
+        monster.setLevel(monster.getLevel() + 1);
+        return monsterRepository.save(monster);
+    }
+
+}
