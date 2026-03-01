4d37db043fbfef51e66116cc3432149a9a3a4a82
Leticia-SS <leticia.saraiva013@gmail.com>
2026-03-01 17:41:16 -0300

    initial commit

---
+++ src/main/java/com/example/mosnterguideapi/MonsterGuideApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mosnterguideapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonsterGuideApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonsterGuideApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mosnterguideapi/MonsterGuideApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mosnterguideapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonsterGuideApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
